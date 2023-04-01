afc8a901e674f23e6d012315af684080b160441f
badstar1004 <dmswjd4566@hanmail.net>
2023-04-01 22:00:06 +0900

    feat : [002-CUSTOMER-BALANCE] Customer(고객)에 대한 잔액 조회 구현.

--- user-api/src/main/java/com/zerobase/user/controller/CustomerController.java
+++ user-api/src/main/java/com/zerobase/user/controller/CustomerController.java
@@ -4,13 +4,17 @@ import static com.zerobase.user.exception.ErrorCode.NOT_FOUND_USER;
 
 import com.zerobase.domain.common.UserVo;
 import com.zerobase.domain.config.JwtAuthenticationProvider;
+import com.zerobase.user.domain.customer.ChangeBalanceForm;
 import com.zerobase.user.domain.customer.CustomerDto;
 import com.zerobase.user.domain.model.Customer;
 import com.zerobase.user.exception.CustomException;
+import com.zerobase.user.service.CustomerBalanceService;
 import com.zerobase.user.service.CustomerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +27,7 @@ public class CustomerController {
     // 현재 토큰이 암호화 되어있음
     private final JwtAuthenticationProvider jwtAuthenticationProvider;
     private final CustomerService customerService;
+    private final CustomerBalanceService customerBalanceService;
 
     @GetMapping("/getInfo")
     public ResponseEntity<CustomerDto> getInfo(@RequestHeader(name = "X-AUTO-TOKEN") String token) {
@@ -33,5 +38,11 @@ public class CustomerController {
         return ResponseEntity.ok(CustomerDto.from(customer));
     }
 
+    @PostMapping("/balance")
+    public ResponseEntity<Integer> changeBalance(@RequestHeader(name = "X-AUTO-TOKEN") String token,
+        @RequestBody ChangeBalanceForm changeBalanceForm){
+        UserVo userVo = jwtAuthenticationProvider.getUserVo(token);
 
+        return ResponseEntity.ok(customerBalanceService.changeBalance(userVo.getId(), changeBalanceForm).getCurrentMoney());
+    }
 }
