8730c58d9960a0569962c29bd21ff7db830c7835
badstar1004 <dmswjd4566@hanmail.net>
2023-04-02 21:09:53 +0900

    feat : [007-PRODUCT-MODIFY] 상품 수정, 상품 아이템 수정 및 스크래치 테스트 작성

--- order-api/src/main/java/com/zerobase/order/service/ProductItemService.java
+++ order-api/src/main/java/com/zerobase/order/service/ProductItemService.java
@@ -1,11 +1,13 @@
 package com.zerobase.order.service;
 
+import static com.zerobase.order.exception.ErrorCode.NOT_FOUND_ITEM;
 import static com.zerobase.order.exception.ErrorCode.NOT_FOUND_PRODUCT;
 import static com.zerobase.order.exception.ErrorCode.SAME_ITEM_NAME;
 
 import com.zerobase.order.domain.model.Product;
 import com.zerobase.order.domain.model.ProductItem;
 import com.zerobase.order.domain.product.AddProductItemForm;
+import com.zerobase.order.domain.product.UpdateProductItemForm;
 import com.zerobase.order.domain.repository.ProductItemRepository;
 import com.zerobase.order.domain.repository.ProductRepository;
 import com.zerobase.order.exception.CustomException;
@@ -20,6 +22,12 @@ public class ProductItemService {
     private final ProductRepository productRepository;
     private final ProductItemRepository productItemRepository;
 
+    /**
+     * 상품 아이템 추가
+     * @param sellerId
+     * @param addProductItemForm
+     * @return
+     */
     @Transactional
     public Product addProductItem(Long sellerId, AddProductItemForm addProductItemForm){
         // Product 가 있는지 확인
@@ -36,5 +44,24 @@ public class ProductItemService {
         product.getProductItemList().add(productItem);
 
         return product;
+    }
+
+    /**
+     * 상품 아이템 수정
+     * @param sellerId
+     * @param updateProductItemForm
+     * @return
+     */
+    @Transactional
+    public ProductItem updateProductItem(Long sellerId, UpdateProductItemForm updateProductItemForm){
+        ProductItem productItem = productItemRepository.findById(updateProductItemForm.getId())
+            .filter(pi -> pi.getSellerId().equals(sellerId))
+                .orElseThrow(() -> new CustomException(NOT_FOUND_ITEM));
+
+        productItem.setName(updateProductItemForm.getName());
+        productItem.setPrice(updateProductItemForm.getPrice());
+        productItem.setCount(updateProductItemForm.getCount());
+
+        return productItem;
     }
 }
